b1cf16fdb55b43461041601c6bee3a4f9785446b
Eder <chlrobert93@gmail.com>
2021-06-14 17:17:29 -0500

    Nuevo componente

--- src/index.js
+++ src/index.js
@@ -1,9 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker  from 'faker';
 
 //Crear componente
 const App = () => {
-    return <div>Hi there</div>
+    return (
+        <div className="ui  container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                  <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam     
+                    </a>
+                    <div className="metadata">
+                       <span className="date"> Today at 6:00pm</span>                    
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt ="avatar" src={faker.image.avatar()}></img>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00pm</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt ="avatar" src={faker.image.avatar()}></img>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00pm</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+
+            
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt ="avatar" src={faker.image.avatar()}></img>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00pm</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+
+        </div>
+    );
 };
+
+if (module.hot) {
+    module.hot.accept();
+  }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
